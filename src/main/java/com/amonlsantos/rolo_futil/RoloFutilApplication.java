143e3dae3beffe49d4bf98e7b08b3eac2bf5c70b
Amon <amonlemes3@gmail.com>
2025-12-22 16:00:52 -0300

    first commit

---
+++ src/main/java/com/amonlsantos/rolo_futil/RoloFutilApplication.java
@@ -0,0 +1,13 @@
+package com.amonlsantos.rolo_futil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoloFutilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoloFutilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amonlsantos/rolo_futil/RoloFutilApplicationTests.java
@@ -0,0 +1,14 @@
+package com.amonlsantos.rolo_futil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoloFutilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
